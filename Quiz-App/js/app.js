fff0b36c9241a7afff376ed40bfc1c3fc4ef43c9
Nigamananda Biswas <nigam@Nigamanandas-MacBook-Air.local>
2021-05-13 21:16:42 +0530

    Inital Commit

---
+++ Quiz-App/js/app.js
@@ -0,0 +1,43 @@
+const quizData = [
+    {
+        question: "How old is Florin?",
+        a: "10",
+        b: "17",
+        c: "26",
+        d: "110",
+        correct: 'c'
+    }, 
+    {
+        question: "What is the most popular programming language?",
+        a: "Python",
+        b: "C++",
+        c: "Java",
+        d: "Javascript",
+        correct: 'a'
+    },
+    {
+        question: "Who is the prime minister of India?",
+        a: "Narendra Modi",
+        b: "Manmohan Singh",
+        c: "Nirmal Jyoti Biswas",
+        d: "DoLund Trump",
+        correct: 'a'
+    },
+    {
+        question: "Why are you gay?",
+        a: "Havok Gae",
+        b: "Havok Gae ftw",
+        c: "Havok not Gae",
+        d: "Havok is Gae",
+        correct: 'a'
+    },
+    {
+        question: "Who is the co-founder of Microsoft?",
+        a: "Paul Alen",
+        b: "Steve Jobs",
+        c: "Nirmal Jyoti Biswas",
+        d: "Brandom Beck"
+    }
+];
+
+console.log(quizData);
