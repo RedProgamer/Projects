ebcf74cce0b3c14b83c4d592e04a526e2f6a7d64
Nigamananda Biswas <nigam@Nigamanandas-MacBook-Air.local>
2021-05-13 18:45:08 +0530

    Completed the project

--- CountDown-Timer/js/app.js
+++ CountDown-Timer/js/app.js
@@ -1,1 +1,34 @@
-console.log(new Date().getSeconds());
+// DOM
+let month = document.querySelector('#month');
+let days = document.querySelector('#days');
+let hours = document.querySelector('#hours');
+let minutes = document.querySelector('#minutes');
+let seconds = document.querySelector('#seconds');
+let specific_timer = document.querySelector('.am-or-pm');
+
+
+let today = '13th May, 2021';
+
+function initialize() {
+
+    //current time
+    let current = new Date();
+    
+    let currentMonth = String(current.getMonth()).padStart(2, '0');
+    month.textContent = currentMonth;
+    
+    let currentDay = current.getDate();
+    days.textContent = currentDay;
+    
+    let currentHour = current.getHours();
+    hours.textContent = currentHour;
+    
+    let currentMinute = current.getMinutes();
+    minutes.textContent = currentMinute;
+    
+    let currentsecond = current.getSeconds();
+    seconds.textContent = currentsecond;
+}
+
+// initialize();
+setInterval(initialize, 1000);
