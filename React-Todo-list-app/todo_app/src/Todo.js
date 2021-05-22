b007ff14d3717ffd3bc997da8a302695182eb2ce
Nirmal Jyoti Biswas <59522492+RedProgamer@users.noreply.github.com>
2021-05-22 10:01:55 +0530

    Added Icon

--- React-Todo-list-app/todo_app/src/Todo.js
+++ React-Todo-list-app/todo_app/src/Todo.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import { List, ListItem, ListItemText, ListItemAvatar, Avatar } from '@material-ui/core';
-import ImageIcon from '@material-ui/icons/Image';
+import FolderIcon from '@material-ui/icons/Folder';
+import PageviewIcon from '@material-ui/icons/Pageview';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import { makeStyles } from '@material-ui/core/styles';
+import { green, pink } from '@material-ui/core/colors';
+
+// import ImageIcon from '@material-ui/icons/Image';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+      '& > *': {
+        margin: theme.spacing(1),
+      },
+    },
+    pink: {
+      color: theme.palette.getContrastText(pink[500]),
+      backgroundColor: pink[500],
+    },
+    green: {
+      color: '#fff',
+      backgroundColor: green[500],
+    },
+  }));
 
 function Todo(props) {
+
+    const classes = useStyles();
+
     return (
         <List>
             <ListItem>
             <ListItemAvatar>
-                <Avatar src="">
+                <Avatar className={classes.green}>
+                    <AssignmentIcon />
                 </Avatar>
             </ListItemAvatar>
                 <ListItemText primary="Todo" secondary={props.text} />
