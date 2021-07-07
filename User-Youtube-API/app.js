5ef4e6916f23d53d38cafd9e4c965dec71e3c2ee
RedProgamer <nirmaljyotib@gmail.com>
2021-07-07 16:34:49 +0530

    Added a new Project

---
+++ User-Youtube-API/app.js
@@ -0,0 +1,1 @@
+const client_id = '998400505735-95e34ccbao638vs4irc3kunkcti0m6ma.apps.googleusercontent.com';
--- spotify-api/js/app.js
+++ spotify-api/js/app.js
@@ -1,18 +1,25 @@
 const my_client_id = 'f20f69e416c344528351a1fa7910ee19';
-const base_url = 'https://api.spotify.com';
+const my_client_secret = 'cfe3e6e9cfb7455e83ad9a6f1c9d6f0e';
+const token_url = 'https://accounts.spotify.com/authorize';
 
-async function authorize(url, client_id) {
-    const response = await fetch(url, {
-        client_id: client_id,
-        method: "GET",
-        response_type: "code"
+const value = `
+curl -X "GET" "" -H "Accept: application/json" -H "Content-Type: application/json" -H ""
+`
+
+async function getPlaylists() {
+
+    const response = await fetch('https://api.spotify.com/v1/me/playlists', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer BQBvDiDsGnFW2dFs_nk9E78ynBWIiUhjmJQ3uFKcYtmYjcPGJgldxU0a5hxqH_bF9TbVwTZaoRKTqMz71nGL08Mr_qdbuKxZL2yOyu4gXG_ssfbwvswoVcI4oNih7DjTzN2uDHqxgyets7CKCX0NA0F_tll7KvCEYnw9mrPT14r0u7KBONFD_6okpX0CRcYYZOjn1wQRBbiNz9E2FXKnFWeNP21GotEAVoz73iDCuzES37avr2ZoeBuAGiwWxQRBpPYHgWIcTmPzpJ3uhj2CtEup8g3JKJp2Q0Lo1chE"curl -X "GET" "https://api.spotify.com/v1/me/playlists" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer BQBvDiDsGnFW2dFs_nk9E78ynBWIiUhjmJQ3uFKcYtmYjcPGJgldxU0a5hxqH_bF9TbVwTZaoRKTqMz71nGL08Mr_qdbuKxZL2yOyu4gXG_ssfbwvswoVcI4oNih7DjTzN2uDHqxgyets7CKCX0NA0F_tll7KvCEYnw9mrPT14r0u7KBONFD_6okpX0CRcYYZOjn1wQRBbiNz9E2FXKnFWeNP21GotEAVoz73iDCuzES37avr2ZoeBuAGiwWxQRBpPYHgWIcTmPzpJ3uhj2CtEup8g3JKJp2Q0Lo1chE'
+        }
     });
 
     const data = await response.json();
-
     return data;
 };
 
-const str = 'values3453534'.encode();
-const str_b64 = base64
-console.log(str);
+getPlaylists().then(resData => console.log(resData));
+
