dce4b46013e9faca3afefa183c4cd4cf5f126e17
RedProgamer <nirmaljyotib@gmail.com>
2021-08-31 18:22:54 +0530

    Finished The project

--- Music Player/app.js
+++ Music Player/app.js
@@ -1,1 +1,116 @@
-console.log("Connected");
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+const audio = document.getElementById('audio');
+const progress= document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+
+// Song titles
+const songs = ['hey', 'summer', 'ukulele'];
+
+// Keep track of songs
+let songIndex = 1;
+
+// Initially load song details
+loadSong(songs[songIndex]);
+
+// Update song details
+function loadSong(songTitle) {
+    title.innerText = songTitle;
+    audio.src = `music/${songTitle}.mp3`;
+    cover.src = `images/${songTitle}.jpg`;
+}
+
+// Pause Song
+function pauseSong() {
+    musicContainer.classList.remove('play');
+    playBtn.querySelector('i.fas').classList.remove('fa-pause');
+    playBtn.querySelector('i.fas').classList.add('fa-play');
+
+    audio.pause();
+}
+
+// Play song
+function playSong() {
+    musicContainer.classList.add('play');
+    playBtn.querySelector('i.fas').classList.remove('fa-play');
+    playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+    audio.play();
+}
+
+function prevSong() {
+
+    songIndex--;
+
+    if(songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+
+    loadSong(songs[songIndex]);
+
+    playSong();
+}
+
+function nextSong() {
+
+    songIndex++;
+
+    if(songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+
+    loadSong(songs[songIndex]);
+
+    playSong();
+}
+
+// Update progress bar
+function updateProgress(e) {
+    const { duration, currentTime } = e.srcElement;
+    const currentProgressPercentage = (currentTime/duration) * 100;
+
+    progress.style.width = `${currentProgressPercentage}%`;
+    // console.table([duration, currentTime]);
+}
+
+// Set progress bar when user clicks
+function setProgress(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+    
+    audio.currentTime = (clickX/width) * duration;
+
+    console.log(width, clickX);
+}
+
+
+// Event Listeners
+playBtn.addEventListener('click', ()=> {
+    const isPlaying = musicContainer.classList.contains('play');
+
+    if(isPlaying) {
+        pauseSong();
+    }else {
+        playSong();
+    }
+});
+
+// Change songs
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+// Timeline update event
+audio.addEventListener('timeupdate', updateProgress);
+
+// Click on progress bar
+progressContainer.addEventListener('click', setProgress);
+
+// Song ends
+audio.addEventListener('ended', nextSong);
