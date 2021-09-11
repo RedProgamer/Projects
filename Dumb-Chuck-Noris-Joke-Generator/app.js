cd788007258ade6738f82bc6878d6b9924ee3a25
RedProgamer <nirmaljyotib@gmail.com>
2021-09-12 01:52:38 +0530

    Made some important changes

--- Dumb-Chuck-Noris-Joke-Generator/app.js
+++ Dumb-Chuck-Noris-Joke-Generator/app.js
@@ -1,67 +1,30 @@
 document.querySelector('form').addEventListener('submit', getJokes);
 
-// function getJokes(e) {
-    
-//     const number = document.querySelector('input[type="number"]').value;
-//     const api_URL = `http://api.icndb.com/jokes/random/${number}`;
-//     const jokeArea = document.querySelector('.list-group');
-
-//     let output = '';
-
-//     fetch(api_URL)
-//         .then((response) => {
-//             return response.json();
-//         })
-//         .then((jokeData) => {
-//             if(jokeData.type == 'success') {
-//                 jokeData.value.forEach(function(joke) {
-//                     output += `
-//                         <li class="list-group-item">${joke.joke}</li>
-//                     `;
-//                     console.log(joke.joke);
-//                 });
-//             }else {
-//                 output += `
-//                     <li class="list-group-item"> Oops, something went wrong </li>
-//                 `
-//             }
-//         })
-//         .then ((JokeData) => {
-//             jokeArea.innerHTML = output;
-//         });
-    
-//     e.preventDefault();
-// }
-
 function getJokes(e) {
     
     const categoryStorage = ['explicit', 'nerdy'];
 
-    console.log(categoryStorage);
-
-
     const number = document.querySelector('input[type="number"]').value;
-    // const api_URL = `http://api.icndb.com/jokes/random/${number}?limitTo=categoryStorage`;
-    const api_URL = `https://icanhazdadjoke.com/`;
+    const api_URL = `http://api.icndb.com/jokes/random/${number}`;
     const jokeArea = document.querySelector('.list-group');
     const selectedCategories = document.querySelectorAll('input[name="category"]:checked');
+
+    let output = "";
     
-    // http://api.icndb.com/jokes/random?limitTo=[nerdy]
-
-    // let output = '';
-
     fetch(api_URL)
         .then((response) => {
             return response.json();
         })
-        .then((jokes) => console.log(jokes));
-
-    selectedCategories.forEach((category) => {
-        categoryStorage.push(category.value);
-    });
-    
-    console.log(categoryStorage);
-    
+        .then(jokes => {
+            console.log(jokes);
+            jokes.value.forEach(function(joke) {
+                output += `<li>${joke.joke}</li>`;
+            });
+        })
+        .then(unUsed => jokeArea.innerHTML = output);
+        
+    console.log(output);
+        
     e.preventDefault();
 }
 
